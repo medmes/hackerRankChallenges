705ff4e126ad124a8c1b9ff826a7dd25dbbf647c
medmes <mohammed.mesaoudi@gmail.com>
2019-09-18 21:51:09 +0100

    Binary Search Tree - BFS Algorithm Traversal using Queue | Printing Level Order.

--- src/main/java/hackerrank/tree/BFSTraversalBinaryTree.java
+++ src/main/java/hackerrank/tree/BFSTraversalBinaryTree.java
@@ -1,17 +1,45 @@
 package hackerrank.tree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class BFSTraversalBinaryTree {
 
     private BFSTraversalBinaryTree() {
 
     }
 
+
+    public static List bfs(BinaryTreeNode root) {
+        Queue<BinaryTreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+        List<Integer> elements = new ArrayList<>();
+        while (!nodeQueue.isEmpty()) {
+            elements.add(nodeQueue.peek().getData());
+            // DeQueue from Queue and update the current Node to next Level nodes
+
+            BinaryTreeNode currentNode = nodeQueue.poll();
+
+            //EnQueue Left Child & Right Child
+            if (currentNode.getLeft() != null) {
+                nodeQueue.add(currentNode.getLeft());
+            }
+            if (currentNode.getRight() != null) {
+                nodeQueue.add(currentNode.getRight());
+            }
+        }
+        return elements;
+    }
+
+
     /**
      * O(n^2)
      * BinaryTreeNode root
      * @param root
      */
-    public static void bfsTraversal(BinaryTreeNode root) {
+    public static void bfsTraversalWithoutQueue(BinaryTreeNode root) {
         int height = HeightOfBinaryTree.height(root);
         for (int i=1; i<=height; i++) {
             printLevel(root, i);
--- src/test/java/hackerrank/tree/BFSTraversalBinaryTreeTest.java
+++ src/test/java/hackerrank/tree/BFSTraversalBinaryTreeTest.java
@@ -6,6 +6,7 @@ import org.junit.runners.JUnit4;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
@@ -14,7 +15,7 @@ import static org.junit.Assert.*;
 public class BFSTraversalBinaryTreeTest {
 
     @Test
-    public void bfsTraversal_shouldPrintByLevelOrder() {
+    public void bfsTraversal_shouldPrintByLevelOrderWithoutQueue() {
         //Given
         BinaryTreeNode root = null;
         root = HeightOfBinaryTree.insert(root, 3);
@@ -30,11 +31,33 @@ public class BFSTraversalBinaryTreeTest {
         System.setOut(new PrintStream(out));
 
         //When
-        BFSTraversalBinaryTree.bfsTraversal(root);
+        BFSTraversalBinaryTree.bfsTraversalWithoutQueue(root);
 
         //Then Check print out:
         assertEquals("3 2 5 1 4 6 7", out);
         System.setOut(save_out);
+    }
+
+    @Test
+    public void bfs_shouldPrintByLevelOrderUsingQueue() {
+        //Given
+        BinaryTreeNode root = null;
+        root = HeightOfBinaryTree.insert(root, 3);
+        root = HeightOfBinaryTree.insert(root, 2);
+        root = HeightOfBinaryTree.insert(root, 5);
+        root = HeightOfBinaryTree.insert(root, 1);
+        root = HeightOfBinaryTree.insert(root, 4);
+        root = HeightOfBinaryTree.insert(root, 6);
+        root = HeightOfBinaryTree.insert(root, 7);
+
+        //When
+        List oderLevelElements = BFSTraversalBinaryTree.bfs(root);
+
+        //Then Check print out:
+        assertThat(oderLevelElements)
+                .isNotNull();
+        assertThat(oderLevelElements)
+                .containsSequence(3, 2, 5, 1, 4, 6, 7);
     }
 
 }
