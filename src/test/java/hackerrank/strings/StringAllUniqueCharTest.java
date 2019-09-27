b63393890abf4cc62a21dd810417add7cbe6aa9b
medmes <mohammed.mesaoudi@gmail.com>
2019-09-27 15:26:36 +0100

    Strings: determine if a string has all unique characters.

--- src/test/java/hackerrank/strings/StringAllUniqueCharTest.java
+++ src/test/java/hackerrank/strings/StringAllUniqueCharTest.java
@@ -25,10 +25,28 @@ public class StringAllUniqueCharTest {
     }
 
     @Test
+    public void isAllUniqueUsingSorting_shouldReturnTrue() {
+
+        //Given + When
+        boolean result = StringAllUniqueChar.isAllUniqueUsingSorting("auCnkMwrp");
+        //Then
+        assertTrue(result);
+    }
+
+    @Test
+    public void isAllUniqueUsingSorting_shouldReturnFalse() {
+
+        //Given + When
+        boolean result = StringAllUniqueChar.isAllUniqueUsingSorting("auCnkMwCrp");
+        //Then
+        assertFalse(result);
+    }
+
+    @Test
     public void isAllUniqueUsingHashTable_shouldReturnTrue() {
 
         //Given + When
-        boolean result = StringAllUniqueChar.isAllUnique("auCnkMwrp");
+        boolean result = StringAllUniqueChar.isAllUniqueUsingHashTable("auCnkMwrp");
         //Then
         assertTrue(result);
     }
@@ -37,7 +55,7 @@ public class StringAllUniqueCharTest {
     public void isAllUniqueUsingHashTable_shouldReturnFalse() {
 
         //Given + When
-        boolean result = StringAllUniqueChar.isAllUnique("auCnkMwCrp");
+        boolean result = StringAllUniqueChar.isAllUniqueUsingHashTable("auCnkMwCrp");
         //Then
         assertFalse(result);
     }
